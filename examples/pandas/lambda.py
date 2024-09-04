37eb9e2cde967e967cd29e8130b18f23ec229488
spyrospav <pavlatos@seas.upenn.edu>
2024-09-04 18:29:49 -0400

    Updated pandas example

--- examples/pandas/lambda.py
+++ examples/pandas/lambda.py
@@ -1,22 +1,19 @@
 
-import logging
 import numpy as np
 import pandas as pd
 
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-logger.info('Loading function')
-
-
 def lambda_handler(event, context):
-    logger.info('numpy version is {}'.format(np.__version__))
     lib_version = {'numpy': np.__version__, 'pandas': pd.__version__}
 
     sales = [{'account': 'Jones LLC', 'Jan': 150, 'Feb': 200, 'Mar': 140},
              {'account': 'Alpha Co', 'Jan': 200, 'Feb': 210, 'Mar': 215},
              {'account': 'Blue Inc', 'Jan': 50, 'Feb': 90, 'Mar': 95}]
     df = pd.DataFrame(sales)
-    logger.info(df)
-    return lib_version
+    return df, lib_version
+
+if __name__ == "__main__":
+    df, lib = lambda_handler(42, 42)
+    print("Dataframe:")
+    print(df)
+    print("Libraries:")
+    print(lib)
