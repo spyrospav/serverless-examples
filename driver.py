46659ad95331895b3f50e5198b25c31d29212977
Spyros Pavlatos <pavlatos@seas.upenn.edu>
2024-11-12 14:13:16 -0500

    Clean up

--- driver.py
+++ driver.py
@@ -1,20 +1,33 @@
+import json
+import argparse
 import importlib
-import os
 
-appname = os.environ['APPNAME']
+if __name__ == "__main__":
 
-modname = appname + '.lambda'
+    parser = argparse.ArgumentParser(
+        description="Driver for running test cases for Lambda functions",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog="Enjoy :)",
+    )
 
-module = importlib.import_module(appname)
+    parser.add_argument("appname", help="Name of the application", type=str)
 
-handler = getattr(module, 'handler')
+    args = parser.parse_args()
 
-# open data.json file
-with open(appname + '/data.json') as f:
-    
-    data = json.load(f)
+    appname = args.appname
 
-    for entry in data:
-        event = entry['event']
-        context = entry['context']
-        print(handler(event, context))
+    modname = appname + ".lambda"
+
+    module = importlib.import_module(appname)
+
+    handler = getattr(module, "handler")
+
+    # open data.json file
+    with open(appname + "/data.json") as f:
+
+        data = json.load(f)
+
+        for entry in data:
+            event = entry["event"]
+            context = entry["context"]
+            print(handler(event, context))
